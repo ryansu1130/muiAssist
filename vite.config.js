fcb8849b88ec5cf98f37e9e18611797c6713e501
Ryan Su <suxi@oregonstate.edu>
2024-02-04 16:29:07 -0800

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/muiAssist/',
+  build:{
+    target: "esnext" // or "es2019",
+   },
 })
